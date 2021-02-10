94dc02eb208e943a87674aa03df7f0cb9395adff
chinmai <me@chinmai.dev>
2021-02-09 23:49:27 -0800

    this code is 100% untested its gonna not work when i wake up and im gonna wonder why

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { secrets } from "./secret";
 import ClientOAuth2 from "client-oauth2";
 import session from "express-session";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuid } from "uuid";
 import http from "http";
 import socketIO from "socket.io";
 import sessionFileStore from "session-file-store";
@@ -129,35 +129,132 @@ passport.deserializeUser((id, done) => {
     .catch((error) => done(error, false));
 });
 
-app.use(
-  session({
-    genid: (req: any) => {
-      console.log("Inside the session middleware");
-      console.log(req.sessionID);
-      return uuidv4(); // use UUIDs for session IDs
-    },
-    store: new FileStore(),
-    secret: secrets.express.secret,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.set("view engine", "pug");
-// Configure Express to use EJS
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static("src"));
-// define a route handler for the default home page
-
-app.get("/", (req, res) => {
-  // render the index template
-  console.log(req.sessionID);
-  res.render("index", { title: "Hey", message: "Hello there!" });
-});
 
 io.on("connection", function (socket: any) {
   console.log("a user connected");
 });
+
+
+
+// add & configure middleware
+app.set("view engine", "pug");
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("src"));
+
+// express stuff
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    genid: (req) => {
+      console.log("Inside session middleware genid function");
+      console.log(`Request object sessionID from client: ${req.sessionID}`);
+      return uuid(); // use UUIDs for session IDs
+    },
+    store: new FileStore(),
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// create the homepage route at '/'
+app.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    console.log("hes already logged in!");
+
+    res.redirect("/rooms");
+  } else {
+    res.render("index", {
+      title: "Hey",
+      message: "You are not logged in. You need to log in kiddo",
+    });
+    console.log("Connect from " + req.sessionID);
+  }
+});
+
+// create the login get and post routes
+// various login pages
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Log in to Shareify",
+    message: req.query.reason,
+    r2: req.query.r2,
+  });
+});
+
+// create account page
+app.get("/create", (req, res) => {
+  res.render("signup", {
+    title: "Create an account",
+    message: "dont use a real password",
+  });
+});
+
+app.post("/login", (req, res, next) => {
+  console.log("in login");
+  passport.authenticate("local", (err, user, info) => {
+    if (info) {
+      console.log(info);
+      console.log("SOmething happened i think");
+      return res.send(info.message);
+    }
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+    req.login(user, (err) => {
+      console.log("err");
+      console.log(err);
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/rooms");
+    });
+  })(req, res, next);
+});
+
+app.post("/signup", (req, res, next) => {
+  console.log("in Signup");
+  return res.redirect("/login" + "?reason=sign%20in" + "&r2=home");
+  //axios.patch(`http://localhost:5000/users/${id}`, { music: "" });
+});
+
+app.get("/rooms", (req:any, res) => {
+  if (req.isAuthenticated()) {
+    // this is the passport uid
+    console.log(req.session.passport.user);
+    let id = req.session.passport.user;
+    let este;
+    let uname;
+    // gets username or something
+    axios.get(`http://localhost:5000/users/${id}`).then((wha) => {
+      console.log(wha);
+      este = wha.data;
+      uname = este.email;
+      if ((este.spotify = "")) {
+        // do the login ting
+      }
+    });
+    res.render("index", { title: "Hey", message: "you are auth!",  });
+  } else {
+    console.log("user is not auth");
+    res.redirect("/login" + "?reason=noauth" + "&r2=rooms");
+  }
+});
+
+// tell the server what port to listen on
+app.listen(3000, () => {
+  console.log("Listening on localhost:3000");
+});
+
 
 var spotifyAuth = new ClientOAuth2({
   clientId: secrets.spotify.id,
